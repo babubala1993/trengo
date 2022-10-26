2d617c0c9a7cfefad40ef6cf8c2759e71174d808
babubala1993 <babubalamurugan93@gmail.com>
2022-10-26 08:54:47 +0200

    updated test

--- cypress/integration/trengo_assignment.spec.js
+++ cypress/integration/trengo_assignment.spec.js
@@ -4,14 +4,14 @@ import { TeamsPage} from '../Pages/TeamsPage';
 describe('Custom channel and team automation', () => {
     const customChannelPage = new CustomChannelPage();
     const teamsPage = new TeamsPage();
-    const accessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMWM2Njc0M2IwZjEwZTI5N2MyMDU2YWMyZmFiMjk5OWI2N2Q0NTg1NWIyNTY1MjA2MzNjOGUyMjBiMGQwMWIyYzRlNDkxMDI5Yjk4NzI2NmUiLCJpYXQiOjE2NjY2Mjk2MzAuMzYxMzExLCJuYmYiOjE2NjY2Mjk2MzAuMzYxMzE1LCJleHAiOjE2OTgxNjU2MzAuMzU1OTc0LCJzdWIiOiI1NTc1MjQiLCJzY29wZXMiOltdfQ.bt3qEVIB8vxUlH6QcRJWoqnMCvIQuanRqc1JDatIa1TyzCzWOOHwp0v_EMVx4kNIHNoXADmL_Ee_ejEc52UsyA';
+    const accessToken = '<<ACCESS TOKEN>>';
     const channelName = "channelAutomation";
     const teamName = "teamAutomation";
 
     beforeEach(function () {
         cy.visit('https://app.trengo.com')
-        cy.get('[name="email"]').type('babubalamurugan93@gmail.com');
-        cy.get('[name="password"]').type('Pass@1602');
+        cy.get('[name="email"]').type('<<EMAIL>>');
+        cy.get('[name="password"]').type('<<PASSWORD>>');
         cy.get('[type="submit"]').click();
     })
 
